6939277e69570ecc189785bdf5b0d567f6c568b9
vipothamse-copart <Vijay.Pothamsetty@copart.com>
2018-03-12 12:24:57 +0530

    Completed 12. Adding Routing to our Burger Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
+import Checkout from './container/Checkout/Checkout';
+import Orders from './container/Orders/Orders';
+
+import {Route,Switch} from 'react-router-dom';
 
 
 class App extends Component {
@@ -8,7 +12,11 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/" exact component={BurgerBuilder}/>        
+        </Switch>
         </Layout>
       </div>
     );
